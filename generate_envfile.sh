79611fe3cd8f794a625888552a4d6cfe81b8dba5
Kwabena Aning <kwabena@mykobo.co>
2025-10-18 10:34:44 +0100

    typo
    Signed-off-by: Kwabena Aning <kwabena@mykobo.co>

--- generate_envfile.sh
+++ generate_envfile.sh
@@ -2,20 +2,20 @@
 
 if [ "${CIRCLE_BRANCH}" != "main" ]
 then
-    echo "VITE_API_BASE_URL=https://dev.mykobo.app" > ./wallet-connetct/.env.production
-    echo "VITE_SOLANA_NETWORK=devnet" >> ./wallet-connetct/.env.production
-    echo "VITE_SOLANA_RPC_URL=https://api.devnet.solana.com" >> ./wallet-connetct/.env.production
-    echo "VITE_ENABLE_ETHEREUM=false" >> ./wallet-connetct/.env.production
-    echo "VITE_ENABLE_SOLANA=true" >> ./wallet-connetct/.env.production
-    echo "VITE_WC_PROJECT_ID=${VITE_WC_PROJECT_ID}" >> ./wallet-connetct/.env.production
+    echo "VITE_API_BASE_URL=https://dev.mykobo.app" > ./wallet-connect/.env.production
+    echo "VITE_SOLANA_NETWORK=devnet" >> ./wallet-connect/.env.production
+    echo "VITE_SOLANA_RPC_URL=https://api.devnet.solana.com" >> ./wallet-connect/.env.production
+    echo "VITE_ENABLE_ETHEREUM=false" >> ./wallet-connect/.env.production
+    echo "VITE_ENABLE_SOLANA=true" >> ./wallet-connect/.env.production
+    echo "VITE_WC_PROJECT_ID=${VITE_WC_PROJECT_ID}" >> ./wallet-connect/.env.production
 
 else
-    echo "VITE_API_BASE_URL=https://mykobo.app" > ./wallet-connetct/.env.production
-    echo "VITE_SOLANA_NETWORK=mainnet" >> ./wallet-connetct/.env.production
-    echo "VITE_SOLANA_RPC_URL=https://api.mainnet-beta.solana.com" >> ./wallet-connetct/.env.production
-    echo "VITE_ENABLE_ETHEREUM=false" >> ./wallet-connetct/.env.production
-    echo "VITE_ENABLE_SOLANA=true" >> ./wallet-connetct/.env.production
-    echo "VITE_WC_PROJECT_ID=${VITE_WC_PROJECT_ID}" >> ./wallet-connetct/.env.production
+    echo "VITE_API_BASE_URL=https://mykobo.app" > ./wallet-connect/.env.production
+    echo "VITE_SOLANA_NETWORK=mainnet" >> ./wallet-connect/.env.production
+    echo "VITE_SOLANA_RPC_URL=https://api.mainnet-beta.solana.com" >> ./wallet-connect/.env.production
+    echo "VITE_ENABLE_ETHEREUM=false" >> ./wallet-connect/.env.production
+    echo "VITE_ENABLE_SOLANA=true" >> ./wallet-connect/.env.production
+    echo "VITE_WC_PROJECT_ID=${VITE_WC_PROJECT_ID}" >> ./wallet-connect/.env.production
 fi
 ls ./wallet-connect
-cat ./wallet-connetct/.env.production
+cat ./wallet-connect/.env.production
